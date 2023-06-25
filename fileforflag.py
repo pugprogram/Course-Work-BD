45955eb3bc17fd38cb60e149c116761ea786eb40
pugprogram <108753375+pugprogram@users.noreply.github.com>
2023-06-25 22:40:56 +0300

    Create fileforflag.py

---
+++ fileforflag.py
@@ -0,0 +1,12 @@
+import sqlite3
+flag=0
+data=""
+def FindFaculty(message):
+	sqlite_connection = sqlite3.connect('sqlite_python.db')
+	cursor = sqlite_connection.cursor()
+	sqlite_query='''SELECT FACULTY_NAME FROM FACULTY
+	WHERE USERNAME=\''''+ message +"\';"
+	cursor.execute(sqlite_query)
+	findmas=cursor.fetchall()
+	cursor.close()
+	return findmas
